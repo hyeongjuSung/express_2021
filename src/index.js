402116783ae5b406c6fc808fe5afec832d7b1d16
hyeongjuSung <tjdgudwn1@gmail.com>
2021-09-07 11:15:11 +0900

    postman_rest_test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000);
 
-app.get("/", (req, res) => {
-    const query = req.query;
-    res.send(query);
+const users = [];
+
+app.get("/users", (req, res) => {
+    res.send(users);
 });
 
-app.get("/test", (req, res) => {
-    res.send("test");
+app.get("/users/:id", (req, res) => {
+    res.send("user "+req.params.id+" get");
 });
 
+app.post("/users", (req, res) => {
+    users.push(req.body);
+    res.send("user add");
+});
+
+app.put("/users/:id", (req, res) => {
+    res.send("user "+req.params.id+" edit");
+});
+
+app.delete("/users/:id", (req, res) => {
+    res.send("user "+req.params.id+" delete");
+});
