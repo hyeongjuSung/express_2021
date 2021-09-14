7616d3dd44de9ac5db3ff5ac4ab446ff8e24f170
scalewide <scalewide@naver.com>
2021-09-14 14:42:34 +0900

    connect-mysql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
-import user_router from './route/users.js'
-import board_router from './route/boards.js'
+import sequelize  from "sequelize";
+import user_router from './route/users.js';
+import board_router from './route/boards.js';
 
 const app = express();
+const seq = new sequelize('express', 'root', '123456', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+seq.authenticate()
+    .then(() => {
+        console.log('db 연결 성공')
+    })
+    .catch(err => {
+        console.error('db 연결 실패', err);
+    });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
