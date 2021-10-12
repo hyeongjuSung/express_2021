9e381251fb240392720c39d4a11e278d66bba3ad
scalewide <scalewide@naver.com>
2021-10-12 19:19:07 +0900

    modify sequelize_sync initDb

--- initDb.js
+++ initDb.js
@@ -3,7 +3,22 @@ import faker from "faker";
 
 import db from "../src/models/index.js"
 faker.locale = "ko";
-const {User, Board} = db;
+const {User, Board, Permission} = db;
+
+const userCount = 1000;
+const boardCount = userCount * 0.3 * 365;
+
+const permissions = [{
+    title: "관리자", level: 0, desc: "관리자 권한"
+}, {
+    title: "게시판 관리자", level: 1, desc: "게시판 관리자 권한"
+}, {
+    title: "사용자 관리자", level: 2, desc: "사용자 관리자 권한"
+}, {
+    title: "일반 사용자", level: 3, desc: "일반 사용자 권한"
+}, {
+    title: "게스트", level: 4, desc: "게스트 권한"
+}]
 
 const getRandomInt = (min, max) => {
     min = Math.ceil(min);
@@ -11,16 +26,28 @@ const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+const permission_sync = async() => {
+    try{
+        for(let i=0; i<permissions.length; i++){
+            const { title, level, desc } = permissions[i];
+            await Permission.create({ title, level, desc })
+        }
+    }catch(err){
+        console.log(err);
+    }
+}
+
 const user_sync = async() => {
     try{
-        // await User.sync({force:true});
         for(let i=0; i<1000; i++){
             const hashpwd = await bcrypt.hash("test1234", 6);
             await User.create({
                 name: faker.name.lastName()+faker.name.firstName(),
                 age: getRandomInt(15,50),
-                password: hashpwd
+                password: hashpwd,
+                permissionId : getRandomInt(1, 5),
             })
+            if(i%100 === 0) console.log(`${i}/${userCount}`)
         }
     }catch(err){
         console.log(err);
@@ -29,13 +56,13 @@ const user_sync = async() => {
 
 const board_sync = async() => {
     try{
-        // await Board.sync({force:true});
-        for(let i=0; i<100; i++){
+        for(let i=0; i<boardCount; i++){
             await Board.create({
                 title: faker.lorem.sentences(1),
                 content: faker.lorem.sentences(10),
-                userId: getRandomInt(1, 1000)
+                userId: getRandomInt(1, userCount)
             })
+            if(i%100 === 0) console.log(`${i}/${boardCount}`)
         }
     }catch(err){
         console.log(err)
@@ -44,6 +71,11 @@ const board_sync = async() => {
 
 db.sequelize.query("SET FOREIGN_KEY_CHECKS = 0", {raw: true}).then(async() => {
 	await db.sequelize.sync({force: true});
+    await permission_sync();
+    console.log("퍼미션 생성 완료");
 	await user_sync();
+    console.log("유저 생성 완료");
 	await board_sync();
+    console.log("게시글 생성 완료");
+    process.exit();
 });
